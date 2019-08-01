4f27cc0a4f20ea6414112e795c79d4aab2c80659
sujiahong <1511877962@qq.com>
2019-08-01 18:23:09 +0800

    修改

--- game-client/cocos-client/assets/Script/scene/HomeClassScene.js
+++ game-client/cocos-client/assets/Script/scene/HomeClassScene.js
@@ -13,19 +13,37 @@ var cls = {};
 cls.extends = cc.Component;
 cls.properties = {
     startButton: cc.Button,
-    userAvatar: cc.Sprite
+    userAvatar: cc.Sprite,
+    nameLabel: cc.Label,
+    strengthAvatar: cc.Sprite,
+    strengthLabel: cc.Label,
+    redAvatar: cc.Sprite,
+    redLabel: cc.Label,
+    rankButton: cc.Button,
+    wasteButton: cc.Button,
+    startPanel: cc.Sprite,
 };
 
 cls.onLoad = function(){
-    console.log(TAG, "onLoad !");
+    console.log(TAG, "onLoad !", wx);
     if (typeof wx === undefined)
         return;
     this.startButton.node.on("click", this.onStart, this);
+    this.rankButton.node.on("click", this.onRank, this);
+    this.wasteButton.node.on("click", this.onWaste, this);
 }
 
 cls.onStart = function(){
     console.log(TAG, "onStart!!");
-    cc.director.loadScene("ClassificationScene");
+    this.startPanel.node.active = true;
+}
+
+cls.onRank = function(){
+    console.log(TAG, "onRank!!");
+}
+
+cls.onWaste = function(){
+    console.log(TAG, "onWaste!!");
 }
 
 cc.Class(cls);
